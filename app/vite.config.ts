c9502e66cbb4a7d5dc624c50d49ec0a57d3d4249
NanguRepo <nanguthenangu@gmail.com>
2024-02-01 12:41:58 +0100

    add vercel adapter

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import adapter from '@sveltejs/adapter-vercel';
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
+  kit: {
+		adapter: adapter({
+			// see below for options that can be set here
+		})
+	}
   plugins: [
     sveltekit(),
     Icons({
