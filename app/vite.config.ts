95a2f4758b435bd78fe027ab6a856f315675dc18
NanguRepo <nanguthenangu@gmail.com>
2024-02-01 13:53:02 +0100

    endlösung

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,6 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import adapter from '@sveltejs/adapter-vercel';
-import Icons from "unplugin-icons/vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  kit: {
-		adapter: adapter({
-			// see below for options that can be set here
-		})
-	}
-  plugins: [
-    sveltekit(),
-    Icons({
-      compiler: "svelte",
-    }),
-  ],
+	plugins: [sveltekit()]
 });
